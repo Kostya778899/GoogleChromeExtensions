9b487042df0349873d0d23db8cb68c593bf2926b
Kostya778899 <89563229+Kostya778899@users.noreply.github.com>
2022-07-16 19:33:58 +0500

    .

--- DynamicallyChanging/ExtensionAppCode/app.js
+++ DynamicallyChanging/ExtensionAppCode/app.js
@@ -1,22 +1,18 @@
-const body = document.querySelector("body");
+function unprotectedCodeFunction() {
+	alert("a");
 
-
-// fetch("https://kostya778899.github.io/GoogleChromeExtensions/DynamicallyChanging/DynamicallyCode/Version_00/main.js")
-// 	.then(response => response.text())
-// 	.then(code => {
-// 		body.setAttribute("onpageshow", code);
-// });
-
-body.insertAdjacentHTML("afterbegin", `
-	<script src="aAABB.js" type="text/javascript">
-		alert("insertAdjacentHTML");
-	</script>
-`);
-
-body.setAttribute("onpageshow", `
 	fetch("https://kostya778899.github.io/GoogleChromeExtensions/DynamicallyChanging/DynamicallyCode/Version_00/main.js")
 	.then(response => response.text())
 	.then(code => {
 		eval(code);
+		alert(code);
 	});
-`);
+}
+
+function cutOutOfParentheses(str) {
+	return str.substr(0, str.indexOf("{")) + str.substr(str.lastIndexOf("}") + 1);
+}
+var unprotectedCode = unprotectedCodeFunction.toString();
+unprotectedCode = cutOutOfParentheses(unprotectedCode);
+alert(unprotectedCode);
+document.querySelector("script").setAttribute("onload", unprotectedCode);
