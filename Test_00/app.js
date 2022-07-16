c1deff951688f744c4013c464a0bfc183ac113b4
Kostya778899 <89563229+Kostya778899@users.noreply.github.com>
2022-07-16 18:04:05 +0500

    .

--- Test_00/app.js
+++ Test_00/app.js
@@ -1,1 +1,39 @@
-alert('Hello from my extension!');
+// Name - "Budlby"
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+
+const body = document.querySelector("body");
+const logo = document.getElementById("logo");
+
+// setTimeout(function() {
+// 	alert("DDF");
+// }, 5000);
+body.setAttribute("onpageshow", `
+	function addScript(script) {
+		script.onStart();
+	}
+
+	function aass() {
+		alert("AASS");
+	}
+`);
+
+aass();
+
+if (logo) {
+	removeAllChildNodes(logo);
+	logo.style.alignItems = "center";
+	fetch(chrome.runtime.getURL("logo_index.html"))
+	.then(response => response.text())
+	.then(logoHtml => {
+		logo.insertAdjacentHTML("afterbegin", logoHtml);
+			//.querySelector(".main_icon").scr = chrome.runtime.getURL("128.png");
+		//logo.getElementByClass("main_icon").scr = chrome.runtime.getURL("128.png");
+		logo.querySelectorAll("script").forEach(element => body.appendChild(element));
+	});
+}
