f8813a2f7ac966e0c1fe64d789db922965f2ef81
Kostya778899 <89563229+Kostya778899@users.noreply.github.com>
2022-07-17 08:55:04 +0500

    .

--- DynamicallyChanging/ExtensionAppCode/app.js
+++ DynamicallyChanging/ExtensionAppCode/app.js
@@ -1,15 +1,16 @@
-function getInParentheses(str) {
-	const startIndex = str.indexOf("{") + 1;
-	const endIndex = str.lastIndexOf("}") - startIndex;
-	return str.substr(startIndex, endIndex);
-}
-
 function unprotectedCodeFunction() {
 	var script = document.createElement("script");
 	script.setAttribute("src", 
 		"https://kostya778899.github.io/GoogleChromeExtensions/DynamicallyChanging/DynamicallyCode/Version_00/main.js");
 	document.body.appendChild(script);
 }
+
+function getInParentheses(str) {
+	const startIndex = str.indexOf("{") + 1;
+	const endIndex = str.lastIndexOf("}") - startIndex;
+	return str.substr(startIndex, endIndex);
+}
+
 
 const unprotectedCode = getInParentheses(unprotectedCodeFunction.toString());
 document.body.setAttribute("onload", unprotectedCode);
