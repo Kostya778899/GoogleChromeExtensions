b197687fafcef68b0886ab7c8c5c0fda5268e816
Kostya778899 <89563229+Kostya778899@users.noreply.github.com>
2022-07-18 19:34:14 +0500

    .

--- App_Base_00/ExtensionAppCode/popup/code.js
+++ App_Base_00/ExtensionAppCode/popup/code.js
@@ -1,18 +1,24 @@
+const logo = document.getElementById("logo");
 const settingsWindow = document.getElementById("settings_window");
 
-function setElementShow(element, show) {
-    if (show) {
-        element.classList.remove("hide");
-        element.classList.add("show");
-    } else {
-        element.classList.remove("show");
-        element.classList.add("hide");
-    }
+
+function tryReplaceArrayElement(array, oldElement, newElement) {
+    const elementIndex = array.indexOf(oldElement);
+    return ~elementIndex ? (array[elementIndex] = newElement, true) : false;
 }
 
-document.getElementById("logo").onclick = () => window.open("https://github.com/Kostya778899");
-//document.getElementsByClassName("open_settings_button").forEach((element) => alert("a"));
+function getEnabled(element) { return !element.classList.contains("disable"); }
+function setEnabled(element, value) {
+    const tags = value ? ("enable", "disable") : ("disable", "enable");
+    element.classList.remove(tags[1]);
+    element.classList.add(tags[0]);
+}
+function executeIfEnabled(element, event) { return getEnabled(element) ? (event(), true) : false; }
+
+
+logo.onclick = () => executeIfEnabled(logo, () => window.open("https://github.com/Kostya778899"));
 document.body.querySelectorAll(".open_settings_button").forEach((element) => element.onclick = () => {
-    setElementShow(settingsWindow, true);
-    setElementShow(document.getElementById("navbar_main"), false);
+    //setElementShow(settingsWindow, true);
+    //setElementShow(document.getElementById("navbar_main"), false);
+    setEnabled(settingsWindow, true);
 });
