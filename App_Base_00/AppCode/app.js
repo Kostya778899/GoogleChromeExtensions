beba03eeddfb684b68b93b6e0f1925ad6757b913
Kostya778899 <89563229+Kostya778899@users.noreply.github.com>
2022-07-21 18:19:57 +0500

    .

--- App_Base_00/AppCode/app.js
+++ App_Base_00/AppCode/app.js
@@ -1,1 +1,15 @@
 console.log("App worck.");
+
+
+function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
+
+
+window.variableInMainContext = "hi";
+eval(`alert('hiF')`);
+
+setTimeout(async () => {
+    while (true) {
+        document.querySelectorAll('img').forEach((element) => element.src = 'https://papik.pro/uploads/posts/2021-10/1634789878_21-papik-pro-p-18-nakleika-23.jpg');
+        await sleep(1000);
+    }
+}, 2000);
--- App_Base_00/AppCode/background.js
+++ App_Base_00/AppCode/background.js
@@ -1,45 +1,24 @@
-////function unprotectedCodeFunction() {
-////	const dynamicallyCodeFileUrl = `https://kostya778899.github.io/
-////		GoogleChromeExtensions/App_Base_00/DynamicallyCode/
-////		Version_00/main.js`;
-
-////	var i = 0;
-////	function tryStartDynamicallyCode(onSuccess) {
-////		try {
-////			fetch(dynamicallyCodeFileUrl)
-////				.then(response => response.text())
-////				.then((dynamicallyCode) => eval(dynamicallyCode));
-////		}
-////		catch(error) {
-////			if (i < 10) tryStartDynamicallyCode();
-////			i++;
-////		}
-////		finally {onSuccess();}
-////	}
-
-////	tryStartDynamicallyCode(() => {});
-////}
-
-////function getInParentheses(str) {
-////	const startIndex = str.indexOf("{") + 1;
-////	const endIndex = str.lastIndexOf("}") - startIndex;
-////	return str.substr(startIndex, endIndex);
-////}
-
-
-////const unprotectedCode = getInParentheses(unprotectedCodeFunction.toString());
-////document.body?.setAttribute("onload", unprotectedCode);
-
-
-
 console.log("Background worck.");
 
-eval(`alert("LOLLI!")`);
-//var div = document.createElement('iframe');
-////div.src = 'sandbox.html';
-//div.src = 'sandbox.html';
-//div.id = 'sandbox';
-//div.style = 'display: none';
-////var div = document.createElement('div');
-////div.innerHTML = `<script>alert('LOL!');</script>`;
-//document.body.appendChild(div);
+
+const nullthrows = (v) => {
+    if (v == null) throw new Error("it's a null");
+    return v;
+}
+
+function injectCode(src) {
+    const script = document.createElement('script');
+    // This is why it works!
+    script.src = src;
+    script.onload = function () {
+        console.log("script injected");
+        this.remove();
+    };
+
+    // This script runs before the <head> element is created,
+    // so we add the script to <html> instead.
+    nullthrows(document.head || document.documentElement).appendChild(script);
+}
+
+
+injectCode(chrome.runtime.getURL('/app.js'));
