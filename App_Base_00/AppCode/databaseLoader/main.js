de6db83bdb278aaab327c77580e727183be1fbce
Kostya778899 <89563229+Kostya778899@users.noreply.github.com>
2022-07-24 22:37:11 +0500

    .

--- App_Base_00/AppCode/databaseLoader/main.js
+++ App_Base_00/AppCode/databaseLoader/main.js
@@ -5,22 +5,24 @@ const databaseUrl = 'https://kostya778899.github.io/GoogleChromeExtensions/App_B
 
 
 function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
-function injectElement(tag, src) {
+function injectElement(tag, src, id) {
     const element = document.createElement(tag);
     document.body.appendChild(element);
     element.src = src;
+    element.id = id;
     element.onload = function () {
         console.log(String.prototype.concat(tag, ' injected ', '(', src, ')'));
         //this.remove();
     };
+    return element;
 }
 
 
 try {
     (async () => {
         while (!document.body) await sleep(50);
-        injectElement('script', databaseUrl + 'code.js');
-        injectElement('iframe', databaseUrl + 'index.html');
+        injectElement('script', databaseUrl + 'code.js', 'custom_extention_database_code');
+        injectElement('iframe', databaseUrl + 'index.html', 'custom_extention_database_index');
     })();
 } catch (e) {
     console.log('database load error: ' + e);
